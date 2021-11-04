67f33cde232b53abee5fd98a4adc23598edc7875
Jef Mitchell <jef@project87.net>
2021-11-04 19:08:03 -0400

    Initial Code.

---
+++ routes/api/workoutRoutes.js
@@ -0,0 +1,12 @@
+const router = require("express").Router();
+const { Workout } = require("../../models");
+
+// GET most recent workout
+
+// POST new workout
+
+// GET combined weight from past 7 workouts
+
+// GET total duration from past 7 workouts
+
+module.exports = router
---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+const uri =
+  "mongodb+srv://jef49:AHIoNT95i1HIOW9a@cluster0.0mkhu.mongodb.net/app?retryWrites=true&w=majority";
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+});
+
+app.use(routes);
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
